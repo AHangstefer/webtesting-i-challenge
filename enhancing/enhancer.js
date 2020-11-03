ac0fad3104f4f8626654533fa0ed1b9082c5af6d
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-11-03 12:17:49 -0500

    setting up

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,17 +5,42 @@ module.exports = {
   get,
 };
 
+// success(item) method that accepts 
+// an item object and returns a new item 
+// object modified according to the
+// rules defined by the client for 
+// enhancement success.
+
 function success(item) {
   return { ...item };
 }
+
+
+// fail(item) method that accepts an
+// item object and returns a new item
+// object modified according to the rules
+// defined by the client for enhancement
+// failure.
 
 function fail(item) {
   return { ...item };
 }
 
+
+//  repair(item) method that accepts an
+// item object and returns a new item
+// with the durability restored to 100.
+// This method is the simplest of the
+// three and would be a good starting
+// point on this project.
+
 function repair(item) {
   return { ...item };
 }
+
+
+// a get() method for use when working
+// on the stretch problem.
 
 function get(item) {
   return { ...item };
